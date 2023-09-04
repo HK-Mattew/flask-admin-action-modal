1170bf846cf75f357480f206e157bcdd96a20e3d
Mattew S <52506836+HK-Mattew@users.noreply.github.com>
2023-09-04 17:15:54 -0300

    Update asm_modelview.py

--- asm_modelview.py
+++ asm_modelview.py
@@ -3,8 +3,7 @@ from flask import (
     redirect,
     url_for,
     flash,
-    request,
-    session
+    request
     )
 
 
@@ -12,13 +11,6 @@ class ActionShowModal:
 
 
     def asm_create_action_modal(self, form, action_name, title_modal='ASM Title'):
-        session.setdefault('ASM_ACTIONS', {})
-        self.ASM_ACTIONS = session['ASM_ACTIONS']
-
-        self.ASM_ACTIONS[action_name] = {
-            'action_name': action_name,
-            'title_modal': title_modal,
-        }
 
         context = {
             'asm_change': True,
@@ -36,14 +28,8 @@ class ActionShowModal:
 
     @expose('/asm_callback/<action_name>/<ids>', methods=['POST'])
     def internal_asm_callback(self, action_name, ids):
-        session.setdefault('ASM_ACTIONS', {})
-        self.ASM_ACTIONS = session['ASM_ACTIONS']
 
-        if self.ASM_ACTIONS.get(action_name):
-            self.asm_callback(action_name, ids.split(','))
-            del self.ASM_ACTIONS
-        else:
-            return 'Non-existent action'
+        self.asm_callback(action_name, ids.split(','))
 
         return redirect(url_for('.index_view'))
 
